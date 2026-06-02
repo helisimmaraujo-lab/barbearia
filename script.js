6b5c31c339971d20f43cc9cadeaf7e4513e15bc9
Helison <helisimmaraujo@gmail.com>
2026-06-02 16:42:44 -0300

    validação de formulário

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-const botaoAgendar = document.querySelector("#botao-agendar");
-botaoAgendar.addEventListener("click", function(){
-    alert("Agendamento realizado com sucesso!")
-});
 
-const botao = document.querySelector('#btn-cor');
+
+const botao = document.querySelector("#btn-cor");
 const logo = document.querySelector(".logo");
 
-botao.addEventListener("click",() => {
+botao.addEventListener("click", () => {
+  logo.style.color = "red";
+});
 
-logo.style.color = "red";
+const Agendar = document.querySelector("#botao-agendar");
 
+Agendar.addEventListener("click", () => {
+  const nome = document.querySelector("#nome").value;
+  const telefone = document.querySelector("#telefone").value;
+  const servico = document.querySelector("#servico").value;
+
+  if (nome === "" || telefone === "" || servico === "") {
+    alert("por favor preecha todos os campos!");
+    return;
+  }
+
+  alert("Agendamento realizado com sucesso!");
 });
